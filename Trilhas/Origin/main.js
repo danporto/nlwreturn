eec160843acb7190b06a39b0f609b86771bf7eeb
Daniel Porto <danielsalesporto@gmail.com>
2022-05-07 22:30:25 -0300

    Congratulations! Stage 5/5...

--- Trilhas/Origin/main.js
+++ Trilhas/Origin/main.js
@@ -3,11 +3,42 @@ window.addEventListener('scroll', onScroll)
 function onScroll () {
   showNavOnScroll();
   showBackToTopButtonOnScroll();
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+  // activateMenuAtCurrentSection(section);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2 
+  // Verficar se a seção passou da linha
+  // Quais dados vou precisar? 
+  //o topo da seção
+  const sectionTop = section.offsetTop;
+  //a altura da seção
+  const sectionHeight = section.offsetHeight;  
+  // o Topo da seção cheogu ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+  // verifciar se a base está abaixo da linha alvo
+  // quais dados vou precisar?  
+  // a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+  // o final da seção pasosu da linha alvo
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+  //limites da seção
+  const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`) //para colocar uma variável 
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
-  var element = document.querySelector("nav")
-  if (scrollY != 0) {
+  let element = document.querySelector("nav")
+  if (scrollY > 0) {
     element.classList.add("scroll")
   } else {
     element.classList.remove("scroll")
@@ -15,7 +46,7 @@ function showNavOnScroll() {
 }
 
 function showBackToTopButtonOnScroll() {
-  var element = document.querySelector("#backToTopButton")
+  let element = document.querySelector("#backToTopButton")
   if (scrollY > 400) {
     element.classList.add("show")
   } else {
@@ -23,15 +54,13 @@ function showBackToTopButtonOnScroll() {
   }
 }
 
-
-
 function openMenu () {
-  var element = document.querySelector("body")
+  let element = document.querySelector("body")
   element.classList.add("menu-expanded")
 }
 
 function closeMenu () {
-  var element = document.querySelector("body")
+  let element = document.querySelector("body")
   element.classList.remove("menu-expanded")
 }
 
@@ -48,4 +77,7 @@ ScrollReveal({
   #about,
   #about header,
   #about .content`);
+
+
+
 
